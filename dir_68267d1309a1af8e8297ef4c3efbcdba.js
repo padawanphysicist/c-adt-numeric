e071cc80bda15a4ead6a686c08ef20120dedbdad
padawanphysicist <vct.santos@protonmail.com>
2023-11-05 20:30:12 +0000

    Deploying to gh-pages from @ padawanphysicist/num.c@68bf5552f781a6c0a9df0abb8d49a8936c0854de 🚀

--- dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -4,7 +4,5 @@ var dir_68267d1309a1af8e8297ef4c3efbcdba =
       [ "ABC", "structABC.html", "structABC" ]
     ] ],
     [ "new.c", "new_8c.html", "new_8c" ],
-    [ "new.h", "new_8h.html", "new_8h" ],
-    [ "num.c", "num_8c.html", "num_8c" ],
-    [ "num.h", "num_8h.html", "num_8h" ]
+    [ "num.c", "num_8c.html", "num_8c" ]
 ];
---
+++ dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,5 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "new.h", "new_8h.html", "new_8h" ],
+    [ "num.h", "num_8h.html", "num_8h" ]
+];
